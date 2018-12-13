d4b1baec6534002764515b16a6c9b50c7016c664
Maryam Tasviri <tasviri.maryam@yahoo.com>
2018-12-13 11:04:05 +0330

    maybe final for this time

--- app/src/main/java/com/maryam/tasviri/myplate/searchResultPage.java
+++ app/src/main/java/com/maryam/tasviri/myplate/searchResultPage.java
@@ -1,10 +1,12 @@
 package com.maryam.tasviri.myplate;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class searchResultPage extends AppCompatActivity {
 
@@ -16,8 +18,29 @@ public class searchResultPage extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.web);
         webView.getSettings().setJavaScriptEnabled(true);
-        //webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.google.com");
+        //webView.getSettings().setLoadWithOverviewMode(true);
+        //webView.getSettings().setUseWideViewPort(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
 
+    }
+
+    public class CustomClient extends WebViewClient{
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Toast.makeText(searchResultPage.this,"started",Toast.LENGTH_LONG).show();
+
+
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+
+            Toast.makeText(searchResultPage.this,"finished",Toast.LENGTH_LONG).show();
+
+        }
     }
 }
